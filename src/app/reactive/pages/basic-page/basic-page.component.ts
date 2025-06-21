f7d8bccb09dc64f8bdeedf8c66393f3ac7d9acdf
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-20 19:55:52 -0600

    FormBuilder y validaciones en formularios básicos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,66 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+  // formBuilder
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  // name: ['', /** validadores síncronos */ [], /** validadores asíncronos */ []],
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  // myForm = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  // isValidField(fieldName: string): boolean | null {
+  //   return (!!this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched);
+  // }
+
+  // getFieldError(fieldName: string): string | null {
+  //   if (!this.myForm.controls[fieldName]) return null;
+
+  //   const errors = this.myForm.controls[fieldName].errors ?? {};
+  //   for (const key of Object.keys(errors)) {
+  //     switch (key) {
+  //       case 'required':
+  //         return 'Este campo es requerido';
+  //       case 'minlength':
+  //         return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
+  //       case 'min':
+  //         return `Valor mínimo de ${errors['min'].min}`;
+  //     }
+  //   }
+
+  //   return null;
+  // }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+
+  }
+
+}
